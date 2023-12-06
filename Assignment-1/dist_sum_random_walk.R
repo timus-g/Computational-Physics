8190faa50fe5622c75684981818a7a9ceec4339a
timus-g <sumit.webcontent@gmail.com>
2023-12-07 00:00:55 +0530

    updating

--- Assignment-1/dist_sum_random_walk.R
+++
@@ -1,7 +0,0 @@
-sums <- read.csv("dist_sum_random_walk.csv", header=FALSE, stringsAsFactors = TRUE)
-summary(sums)
-pdf("dist_sum_random_walk_test.pdf") # create a pdf file to save the plot
-
-hist(sums)
-
-dev.off() # close the pdf file
